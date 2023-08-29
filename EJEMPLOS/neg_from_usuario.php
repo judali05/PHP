09e73860255e8e6ebd0ae584099872615b999d81
Julian Rojas <129390687+judali05@users.noreply.github.com>
2023-08-29 10:29:33 -0500

    Update neg_from_usuario.php

--- EJEMPLOS/neg_from_usuario.php
+++ EJEMPLOS/neg_from_usuario.php
@@ -1,1 +1,20 @@
+<?php
 
+class usuario{
+
+    public function registrar($nom_usuario, $ape_usuario, $doc_usuario, $celular){
+        
+        include('conexion.php');
+        mysqli_query($db,"INSERT INTO usuarios(nom_usuario, ape_usuario, doc_usuario, celular) VALUES ('$nom_usuario','$ape_usuario','$doc_usuario','$celular')") ;
+        echo"Registro exitoso";
+
+    }
+
+}
+$final = new usuario();
+$final -> registrar($_POST["nom_usuario"], $_POST["ape_usuario"], $_POST["doc_usuario"], $_POST["celular"]);
+
+echo "<br>"; 
+echo "<a href='pres_form_usuario.php'>regresar</a>";
+
+?>
